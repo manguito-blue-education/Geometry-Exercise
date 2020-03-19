1c43fd3e56d8e15b5a4fd5c54a0e81ebb68ec5ae
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-03-19 16:14:07 -0600

    Added exercises

---
+++ geometry.js
@@ -0,0 +1,17 @@
+function getTriangleArea(height, width) {
+  /* Your code here */
+}
+
+function getSquareArea(side) {
+  /* Your code here */
+}
+
+function getCircleArea(radius) {
+  /* Your code here */
+}
+
+module.exports = {
+  getTriangleArea,
+  getSquareArea,
+  getCircleArea
+};

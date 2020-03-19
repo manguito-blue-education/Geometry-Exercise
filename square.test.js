3ac6ac2a3c8fd331304f414d61ea97da672806bd
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-03-19 16:33:04 -0600

    Thinked about andother edge case

--- square.test.js
+++ square.test.js
@@ -1,7 +1,7 @@
 const { getSquareArea } = require("./geometry");
 
 test("Square Side => 3", () => {
-  expect(getSquareArea(3)).toBe(9);
+  expect(getSquareArea(3)).toBe(null);
 });
 
 test("Square Side => 12", () => {
